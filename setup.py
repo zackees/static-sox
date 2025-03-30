f2241dd6ecbdfac9236db476bf19a1f4fb96efa8
Zachary Vorhies <zachvorhies@protonmail.com>
2025-03-30 19:11:38 -0400

    chore: remove MIT license and update package metadata

--- setup.py
+++ setup.py
@@ -12,25 +12,9 @@ KEYWORDS = "sox static binaries pysox"
 HERE = os.path.dirname(os.path.abspath(__file__))
 
 
-def get_readme() -> str:
-    """Get the contents of the README file."""
-    readme = os.path.join(HERE, "README.md")
-    with open(readme, encoding="utf-8", mode="r") as readme_file:
-        readme_lines = readme_file.readlines()
-    for i, line in enumerate(readme_lines):
-        if "../../" in line:
-            # Transform the relative links to absolute links
-            output_string = re.sub(r"(\.\./\.\.)", f"{URL}", line, count=1)
-            output_string = re.sub(r"(\.\./\.\.)", f"{URL}", output_string)
-            readme_lines[i] = output_string
-    return "".join(readme_lines)
-
-
 if __name__ == "__main__":
     setup(
         maintainer="Zachary Vorhies",
         keywords=KEYWORDS,
-        long_description=get_readme(),
-        long_description_content_type="text/markdown",
         url=URL,
         include_package_data=True)
--- upload_package.sh
+++ upload_package.sh
@@ -4,7 +4,7 @@ rm -rf build dist
 . ./activate.sh
 pip install wheel twine
 echo "Building Source and Wheel (universal) distribution…"
-python setup.py sdist bdist_wheel --universal
+python3 setup.py bdist_wheel --universal
 echo "Uploading the package to PyPI via Twine…"
 twine upload dist/* --verbose
 # echo Pushing git tags…
