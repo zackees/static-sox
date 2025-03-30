914d52cd71dfeced4d7bb68b26eb17750b51995a
Zachary Vorhies <zachvorhies@protonmail.com>
2025-03-30 19:28:14 -0400

    Update upload_package.sh with uv script commands

--- upload_package.sh
+++ upload_package.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -e
 rm -rf build dist
-. ./activate.sh
-pip install wheel twine
-echo "Building Source and Wheel (universal) distribution…"
-python3 setup.py bdist_wheel --universal
-echo "Uploading the package to PyPI via Twine…"
-twine upload dist/* --verbose
+uv pip install wheel twine
+uv build --wheel
+uv run twine upload dist/* --verbose
 # echo Pushing git tags…
